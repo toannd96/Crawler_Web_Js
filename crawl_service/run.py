0b3036bb6bfb68e736db982726bfd89ee4486717
Nguyễn Đắc Toàn <30865156+dactoankmapydev@users.noreply.github.com>
2018-11-06 14:25:20 +0700

    run

---
+++ crawl_service/run.py
@@ -0,0 +1,24 @@
+import logging
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from scrapy.utils.log import configure_logging
+from twisted.internet.error import ReactorNotRestartable
+from spiders.product import ProductSpider
+
+configure_logging(install_root_handler=False)
+logging.basicConfig(
+    filename='crawl.log',
+    format='%(levelname)s: %(message)s',
+    level=logging.WARNING)
+
+if __name__ == '__main__':
+
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(ProductSpider)
+
+    try:
+        process.start()
+    except ReactorNotRestartable as e:
+        pass
+    finally:
+        process.stop()
