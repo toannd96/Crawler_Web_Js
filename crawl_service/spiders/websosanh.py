d115e89f5990d1fa89746e9d4ecfa17c8cb14840
Nguyễn Đắc Toàn <30865156+dactoankmapydev@users.noreply.github.com>
2018-11-06 23:31:52 +0700

    Update websosanh.py

--- crawl_service/spiders/websosanh.py
+++ crawl_service/spiders/websosanh.py
@@ -4,18 +4,16 @@ from scrapy_splash import SplashRequest
 
 
 class CrawlServiceItem(scrapy.Item):
-    price = scrapy.Field()
     name = scrapy.Field()
+    price = scrapy.Field()
     image = scrapy.Field()
 
 
 class WebsosanhSpider(scrapy.Spider):
-    name = 'wss'
-    allowed_domains = ['websosanh.vn']
+    name = "wss"
+    allowed_domains = ["websosanh.vn"]
 
-    start_urls = [
-        "https://websosanh.vn/socola/cat-2053.htm"
-    ]
+    start_urls = ["https://websosanh.vn/socola/cat-2053.htm"]
 
     script = """
         function main(splash)
@@ -30,22 +28,23 @@ class WebsosanhSpider(scrapy.Spider):
         """
 
     def start_requests(self):
-
         for url in self.start_urls:
-            yield SplashRequest(url, endpoint='render.html', callback=self.parse)
+            yield SplashRequest(url, endpoint="render.html", callback=self.parse)
 
     def parse(self, response):
         item = CrawlServiceItem()
         for data in response.xpath("//li[@class='item ']"):
-            item['price'] = data.xpath("./div[2]/text()").extract_first()
-            item['name'] = data.xpath("./h3/a/text()").extract_first()
-            item['image'] = data.xpath("./div[1]/a/img[1]/@data-src").extract_first()
-
+            item["name"] = data.xpath("./h3/a/text()").extract_first()
+            if item["name"] == None:
+                item["name"] = data.xpath("./h2/a/text()").extract_first()
+            item["price"] = data.xpath("./div[2]/text()").extract_first()
+            item["image"] = data.xpath("./div[1]/a/img[1]/@data-src").extract_first()
             yield item
 
-        yield SplashRequest(url=response.url, callback=self.parse, meta={
-            'splash': {
-                'endpoint': 'execute',
-                'args': {'lua_source': self.script},
-            }
-        })
+        yield SplashRequest(
+            url=response.url,
+            callback=self.parse,
+            meta={
+                "splash": {"endpoint": "execute", "args": {"lua_source": self.script}}
+            },
+        )
