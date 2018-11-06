15213314ba048039263e5ca37ef77ad3b6b4e30f
Nguyễn Đắc Toàn <30865156+dactoankmapydev@users.noreply.github.com>
2018-11-06 14:15:26 +0700

    Update product.py

--- crawl_service/spiders/product.py
+++ crawl_service/spiders/product.py
@@ -3,10 +3,10 @@ import scrapy
 from scrapy_splash import SplashRequest
 
 
-
-#class CrawlServiceItem(scrapy.Item):
-    #name = scrapy.Field()
-    #image = scrapy.Field()
+class CrawlServiceItem(scrapy.Item):
+    price = scrapy.Field()
+    name = scrapy.Field()
+    image = scrapy.Field()
 
 
 class ProductSpider(scrapy.Spider):
@@ -14,19 +14,14 @@ class ProductSpider(scrapy.Spider):
     allowed_domains = ['websosanh.vn']
 
     start_urls = [
-        #"https://websosanh.vn/dien-lanh/cat-1867.htm",
-        "https://websosanh.vn/sach/cat-216.htm",
+        "https://websosanh.vn/socola/cat-2053.htm"
     ]
 
     script = """
         function main(splash)
             local url = splash.args.url
             assert(splash:go(url))
-            assert(splash:wait(1))
-
             assert(splash:runjs("$('.next')[0].click();"))
-            assert(splash:wait(1))
-
             return {
                 html = splash:html(),
                 url = splash:url(),
@@ -34,34 +29,23 @@ class ProductSpider(scrapy.Spider):
         end
         """
 
-
     def start_requests(self):
-        
+
         for url in self.start_urls:
             yield SplashRequest(url, endpoint='render.html', callback=self.parse)
 
-
     def parse(self, response):
-        #item = CrawlServiceItem()
-        for data in response.css('li.item'):
-        #for data in response.xpath("//*[@class='item ']"):
-            #item['name'] = response.css('h3.title').extract_first(),
-            #item['image'] = response.css('img.lazyload').extract_first()
-            #yield item
-            yield {
-                'name': data.css('h3.title').extract_first(),
-                #'name': response.xpath("//*[@class='title']//text()").extract_first(),
-                #'price': data.xpath("//*[@class='price ']/text()").extract_first(),
-                #'image': data.xpath("//div[@class='img-wrap lazyload']/a/img[@class='lazyload']/@data-src").extract_first()
-            }
+        item = CrawlServiceItem()
+        for data in response.xpath("//li[@class='item ']"):
+            item['price'] = data.xpath("./div[2]/text()").extract_first()
+            item['name'] = data.xpath("./h3/a/text()").extract_first()
+            item['image'] = data.xpath("./div[1]/a/img[1]/@data-src").extract_first()
 
-        #else:
-            #yield SplashRequest(url=response.url, endpoint='execute', args={'lua_source': self.script, 'wait': 1},
-            #        callback=self.parse)
+            yield item
 
         yield SplashRequest(url=response.url, callback=self.parse, meta={
-                'splash': {
-                    'endpoint': 'execute',
-                    'args': {'lua_source': self.script}
-                    }
-                })
+            'splash': {
+                'endpoint': 'execute',
+                'args': {'lua_source': self.script},
+            }
+        })
